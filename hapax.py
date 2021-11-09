d8c117e05f0c55ea26d861f295cd485e55684bb0
jessica-sie <jessica.sie@binus.ac.id>
2021-11-09 11:47:04 +0700

    initial commit

---
+++ hapax.py
@@ -0,0 +1,37 @@
+import re
+def hapax():
+    fileN = input("enter file name: ")
+    file = open(fileN, "r")
+    contents = file.read()
+    # print(contents)#test
+
+    # initialize dictionary to store the words 
+    wordDict={}
+
+    # make lower case
+    contents = contents.lower()
+    # print(contents)#test
+
+    # replacing punctuation with space 
+    contents = re.sub(r'(?u)[^\w\s]', '', contents)
+    
+   
+
+    splitCon = contents.split() # [list,list2]
+    for word in splitCon:
+        wordDict[word] = wordDict.get(word,0)+ 1
+
+    liHapax=[]
+    #append single keys into list 
+    for key, value in wordDict.items():
+        if value == 1:
+            liHapax.append(key)
+
+    print(liHapax)
+            
+
+    file.close
+
+
+hapax()
+
